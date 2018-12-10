6f622e7d6e1837ce08e1ac96454e3a2126cedcfa
heatherlp <heatherpollard0@gmail.com>
2018-12-10 11:31:03 +0000

    Allow install from open workspace project
    Signed-off-by: heatherlp <heatherpollard0@gmail.com>

--- client/integrationTest/integration.test.ts
+++ client/integrationTest/integration.test.ts
@@ -226,7 +226,10 @@ describe('Integration Test', () => {
 
         showInstallableStub.resolves({
             label: name,
-            data: packageToInstall
+            data: {
+                packageEntry: packageToInstall,
+                workspace: undefined
+            }
         });
         await vscode.commands.executeCommand('blockchainExplorer.installSmartContractEntry');
     }
