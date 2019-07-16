aa4b702227d7d5e947fb32cf0380be29696b99f5
Caroline Fletcher <cazfletch@gmail.com>
2019-07-16 09:53:21 +0100

    Fix travis build (#1149)
    Use version 1.35.1 for unit tests
    Updated rewritePackageJson to edit engine
    Put code coverage back in
    moved npm package to before unit test
    
    Signed-off-by: Caroline Fletcher <caroline.fletcher@uk.ibm.com>

--- client/test/runTests.ts
+++ client/test/runTests.ts
@@ -13,9 +13,7 @@
 */
 
 import * as path from 'path';
-import * as glob from 'glob';
-
-import { runTests, downloadAndUnzipVSCode} from 'vscode-test';
+import { runTests, downloadAndUnzipVSCode } from 'vscode-test';
 
 async function main(): Promise<void> {
     try {
@@ -37,38 +35,13 @@ async function main(): Promise<void> {
         // Passed to --extensionTestsPath
         const extensionTestsPath: string = path.resolve(__dirname);
 
-        console.log('downloadin vscode');
-        await downloadAndUnzipVSCode('stable');
+        console.log('downloading vscode');
+        await downloadAndUnzipVSCode('1.35.1');
 
         // Download VS Code, unzip it and run the integration test
         console.log('setting up tests');
 
-        const testsRoot: string = __dirname;
-
-        console.log('finding files', testsRoot);
-        const files: string[] = glob.sync('**/**.test.js', { cwd: testsRoot });
-        console.log('found lots of files', files);
-
-        function chunkArray(myArray: any[], chunkSize: number): any[] {
-            let index: number = 0;
-            const arrayLength: number = myArray.length;
-            const tempArray: any[] = [];
-
-            for (index = 0; index < arrayLength; index += chunkSize) {
-                const myChunk: any[] = myArray.slice(index, index + chunkSize);
-                tempArray.push(myChunk);
-            }
-
-            return tempArray;
-        }
-
-        const results: any[] = chunkArray(files, 10);
-
-        for (const result in results) {
-            console.log('running batch ' + result);
-            process.env.BATCH_NUMBER = result;
-            await runTests({ extensionDevelopmentPath, extensionTestsPath});
-        }
+        await runTests({extensionDevelopmentPath, extensionTestsPath, version: '1.35.1'});
     } catch (err) {
         console.error('Failed to run tests', err);
         process.exit(1);
