c89116c9fbf8964c57550c9178a2de1e9ca3fe28
Caroline Fletcher <caroline.fletcher@uk.ibm.com>
2019-07-31 14:06:56 +0100

    Discover electron version
    Discover the version of electron so we automatically update grpc and don't have to hard code paths
    
    closes #1134
    
    Signed-off-by: Caroline Fletcher <caroline.fletcher@uk.ibm.com>

--- client/src/dependencies/DependencyManager.ts
+++ client/src/dependencies/DependencyManager.ts
@@ -13,13 +13,12 @@
 */
 'use strict';
 
-import { Dependency } from './Dependency';
 import { ExtensionUtil } from '../util/ExtensionUtil';
-
 import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as vscode from 'vscode';
-
+import * as semver from 'semver';
+import Axios from 'axios';
 import { VSCodeBlockchainOutputAdapter } from '../logging/VSCodeBlockchainOutputAdapter';
 import { CommandUtil } from '../util/CommandUtil';
 import { TemporaryCommandRegistry } from './TemporaryCommandRegistry';
@@ -33,7 +32,7 @@ export class DependencyManager {
 
     private static _instance: DependencyManager = new DependencyManager();
 
-    private dependencies: Array<Dependency> = [];
+    private dependencies: Array<string> = [];
 
     private constructor() {
 
@@ -43,7 +42,7 @@ export class DependencyManager {
     public async requireNativeDependencies(): Promise<void> {
         const outputAdapter: VSCodeBlockchainOutputAdapter = VSCodeBlockchainOutputAdapter.instance();
         const packageJSON: any = await this.getRawPackageJson();
-        const nativeModules: string[] = Object.keys(packageJSON.nativeDependencies);
+        const nativeModules: string[] = packageJSON.nativeDependencies;
         for (const _module of nativeModules) {
             outputAdapter.log(LogType.INFO, undefined, `Attempting to require dependency: ${_module}`);
             require(_module);
@@ -87,25 +86,9 @@ export class DependencyManager {
     }
 
     private loadDependencies(): void {
-        const os: string = `${process.platform}-${process.arch}`;
         const packageJSON: any = ExtensionUtil.getPackageJSON();
 
-        this.dependencies = [];
-
-        const dependencyKeys: Array<string> = Object.keys(packageJSON.nativeDependencies);
-
-        for (const moduleName of dependencyKeys) {
-            const module: string = packageJSON.nativeDependencies[moduleName];
-            const moduleOS: any = module[os];
-
-            const dependency: Dependency = new Dependency();
-
-            dependency.moduleName = moduleName;
-            dependency.original = moduleOS.original;
-            dependency.new = moduleOS.new;
-
-            this.dependencies.push(dependency);
-        }
+        this.dependencies = packageJSON.nativeDependencies;
     }
 
     private async installNativeDependenciesInternal(): Promise<void> {
@@ -115,38 +98,104 @@ export class DependencyManager {
             location: vscode.ProgressLocation.Notification,
             title: 'IBM Blockchain Platform Extension',
             cancellable: false
-        }, async (progress: vscode.Progress<{message: string}>) => {
+        }, async (progress: vscode.Progress<{ message: string }>) => {
 
             const outputAdapter: VSCodeBlockchainOutputAdapter = VSCodeBlockchainOutputAdapter.instance();
 
+            const architecture: string = process.arch; // Returns the architecture Code is running on
+            const os: string = process.platform;
+            let thing: string;
+            if (os === 'linux') {
+                thing = 'glibc';
+            } else {
+                thing = 'unknown';
+            }
+
+            const info: { modules: string, longVersion: string, shortVersion: string } = await this.getRebuildInfo(os, architecture, thing);
+
             outputAdapter.log(LogType.INFO, undefined, 'Updating native node modules');
-            progress.report({message: 'Updating native node modules'});
+            progress.report({ message: 'Updating native node modules' });
 
             for (const dependency of this.dependencies) {
                 outputAdapter.log(LogType.INFO, undefined, 'Rebuilding native node modules');
-                progress.report({message: 'Rebuilding native node modules'});
+                progress.report({ message: 'Rebuilding native node modules' });
 
                 // npm needs to run in a shell on Windows
                 const shell: boolean = (process.platform === 'win32') ? true : false;
 
                 try {
-                    const architecture: string = process.arch; // Returns the architecture Code is running on
-                    await CommandUtil.sendCommandWithOutput('npm', ['rebuild', dependency.moduleName, '--target=4.1.5', '--runtime=electron', '--dist-url=https://atom.io/download/electron', '--update-binary', '--fallback-to-build', `--target_arch=${architecture}`], extensionPath, null, outputAdapter, shell);
+                    await CommandUtil.sendCommandWithOutput('npm', ['rebuild', dependency, `--target=${info.longVersion}`, '--runtime=electron', '--dist-url=https://atom.io/download/electron', '--update-binary', '--fallback-to-build', `--target_arch=${architecture}`], extensionPath, null, outputAdapter, shell);
 
                 } catch (error) {
                     outputAdapter.log(LogType.ERROR, `Could not rebuild native dependencies ${error.message}. Please ensure that you have node and npm installed`);
                     throw error;
                 }
 
-                progress.report({message: `Updating ${dependency.moduleName}`});
-                outputAdapter.log(LogType.INFO, undefined, `Updating ${dependency.moduleName}`);
-                await fs.remove(`${extensionPath}/${dependency.original}`);
-                await fs.rename(`${extensionPath}/${dependency.new}`, `${extensionPath}/${dependency.original}`);
+                progress.report({ message: `Updating ${dependency}` });
+                outputAdapter.log(LogType.INFO, undefined, `Updating ${dependency}`);
+                const basePath: string = path.join(extensionPath, 'node_modules', 'grpc', 'src', 'node', 'extension_binary');
+
+                const origPath: string = path.join(basePath, `node-v${info.modules}-${os}-${architecture}-${thing}`);
+                const newPath: string = path.join(basePath, `electron-v${info.shortVersion}-${os}-${architecture}-${thing}`);
+
+                const exists: boolean = await fs.pathExists(origPath);
+                if (exists) {
+                    await fs.remove(origPath);
+                }
+                await fs.rename(newPath, origPath);
             }
 
             outputAdapter.log(LogType.SUCCESS, undefined, 'Finished updating native node modules');
-            progress.report({message: 'Finished updating native node modules'});
+            progress.report({ message: 'Finished updating native node modules' });
         });
+    }
+
+    private async getRebuildInfo(os: string, arch: string, thing: string): Promise<{ modules: string, longVersion: string, shortVersion: string }> {
+        try {
+            const response: any = await Axios.get('https://raw.githubusercontent.com/electron/releases/master/lite.json');
+            let info: any[] = response.data;
+            const modules: string = process.versions.modules;
+
+            info = info.filter((_info: any) => {
+                return _info && _info.deps && _info.deps.modules === modules;
+            });
+
+            const filteredVersions: any[] = [];
+
+            for (const _info of info) {
+                const tempVersion: string = `${semver.major(_info.version)}.${semver.minor(_info.version)}`;
+                const found: { longVersion: string, shortVersion: string } = filteredVersions.find((_version: { longVersion: string, shortVersion: string }) => _version.shortVersion === tempVersion);
+                if (!found) {
+                    filteredVersions.push({ longVersion: _info.version, shortVersion: tempVersion });
+                }
+            }
+
+            if (filteredVersions.length === 0) {
+                throw new Error(`no matching electron versions for modules ${modules}`);
+            }
+
+            let version: { longVersion: string, shortVersion: string };
+            for (const _version of filteredVersions) {
+                try {
+                    const preBuiltBinarypath: string = `https://node-precompiled-binaries.grpc.io/grpc/v1.21.1/electron-v${_version.shortVersion}-${os}-${arch}-${thing}.tar.gz`;
+                    await Axios.get(preBuiltBinarypath);
+                    // found one that exists so use it
+                    version = _version;
+                    break;
+                } catch (error) {
+                    // don't care about the error here as if error then it probably doesn't exist
+                }
+            }
+
+            if (!version) {
+                // didn't find a prebuilt one so just pick the first and use that
+                version = filteredVersions[0];
+            }
+
+            return { modules: modules, longVersion: version.longVersion, shortVersion: version.shortVersion };
+        } catch (error) {
+            throw new Error(`Could not get electron verion, ${error.message}`);
+        }
     }
 
     private async getRawPackageJson(): Promise<any> {
