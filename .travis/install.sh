e5d16a9a606deacca4ee26c969e18dc8d2e1c3b8
Caroline Fletcher <caroline.fletcher@uk.ibm.com>
2019-07-31 17:41:09 +0100

    Run against insiders
    Update to run against insiders
    Also run against 1.32.3
    Change min version of vscode to 1.32.3
    
    contributes to #1022
    
    Signed-off-by: Caroline Fletcher <caroline.fletcher@uk.ibm.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -31,21 +31,24 @@ npm audit
 npm run compile
 
 if [ "${TASK}" == "unit" ]; then
+    TARGET=3.0.0
+    ELECTRON=3.0
     if [ "${VERSION}" == "insiders" ]; then
-        npm rebuild grpc --target=4.1.5 --runtime=electron --dist-url=https://atom.io/download/electron
-    else
-        npm rebuild grpc --target=3.0.0 --runtime=electron --dist-url=https://atom.io/download/electron
+        TARGET=4.1.5
+        ELECTRON=4.1
     fi
+
+    npm rebuild grpc --target=$TARGET --runtime=electron --dist-url=https://atom.io/download/electron
 
     if [ $TRAVIS_OS_NAME == "linux" ]; then
         export CXX="g++-4.9" CC="gcc-4.9" DISPLAY=:99.0;
         rm -rf ./node_modules/grpc/src/node/extension_binary/node-v64-linux-x64-glibc
-        mv ./node_modules/grpc/src/node/extension_binary/electron-v3.0-linux-x64-glibc ./node_modules/grpc/src/node/extension_binary/node-v64-linux-x64-glibc
+        mv ./node_modules/grpc/src/node/extension_binary/electron-v${ELECTRON}-linux-x64-glibc ./node_modules/grpc/src/node/extension_binary/node-v64-linux-x64-glibc
     elif [ $TRAVIS_OS_NAME == "windows" ]; then
         rm -rf ./node_modules/grpc/src/node/extension_binary/node-v64-win32-x64-unknown
-        mv ./node_modules/grpc/src/node/extension_binary/electron-v3.0-win32-x64-unknown ./node_modules/grpc/src/node/extension_binary/node-v64-win32-x64-unknown
+        mv ./node_modules/grpc/src/node/extension_binary/electron-v${ELECTRON}-win32-x64-unknown ./node_modules/grpc/src/node/extension_binary/node-v64-win32-x64-unknown
     else
         rm -rf ./node_modules/grpc/src/node/extension_binary/node-v64-darwin-x64-unknown
-        mv ./node_modules/grpc/src/node/extension_binary/electron-v3.0-darwin-x64-unknown ./node_modules/grpc/src/node/extension_binary/node-v64-darwin-x64-unknown
+        mv ./node_modules/grpc/src/node/extension_binary/electron-v${ELECTRON}-darwin-x64-unknown ./node_modules/grpc/src/node/extension_binary/node-v64-darwin-x64-unknown
     fi
 fi
