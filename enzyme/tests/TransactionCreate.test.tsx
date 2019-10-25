b6534a25d975241bc7ea9377d41c9b21a9d4a757
Erin Hughes <Erin.Hughes@ibm.com>
2019-10-25 09:37:47 +0100

    Created skeleton screen for creating a new transaction
    Signed-off-by: Erin Hughes <Erin.Hughes@ibm.com>

--- enzyme/tests/TransactionCreate.test.tsx
+++ enzyme/tests/TransactionCreate.test.tsx
@@ -1,29 +1,28 @@
+
 import React from 'react';
 import renderer from 'react-test-renderer';
-import TransactionViewSidebar from '../../src/components/TransactionViewSidebar/TransactionViewSidebar';
+import TransactionCreate from '../../src/components/TransactionCreate/TransactionCreate';
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 chai.should();
 chai.use(sinonChai);
 
-describe('TransactionViewSidebar component', () => {
-
-    let mySandBox: sinon.SinonSandbox;
+describe('TransactionCreate component', () => {
+    let mySandbox: sinon.SinonSandbox;
 
     beforeEach(async () => {
-        mySandBox = sinon.createSandbox();
+        mySandbox = sinon.createSandbox();
     });
 
     afterEach(async () => {
-        mySandBox.restore();
+        mySandbox.restore();
     });
 
     it('should render the expected snapshot', async () => {
         const component: any = renderer
-            .create(<TransactionViewSidebar/>)
+            .create(<TransactionCreate/>)
             .toJSON();
         expect(component).toMatchSnapshot();
     });
-
 });
