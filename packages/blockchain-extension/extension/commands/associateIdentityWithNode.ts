ea0c21f510236afb13cc3d274ebdbcd3c3c64840
Caroline Fletcher <cazfletch@gmail.com>
2019-12-03 10:45:24 +0000

    Merge master into ops tools branch
    Merge master and fix conflicts
    
    Signed-off-by: Caroline Fletcher <cazfletch@gmail.com>

--- packages/blockchain-extension/extension/commands/associateIdentityWithNode.ts
+++ packages/blockchain-extension/extension/commands/associateIdentityWithNode.ts
@@ -171,7 +171,7 @@ export async function associateIdentityWithNode(replace: boolean = false, enviro
         if (!yesnoPick || yesnoPick === UserInputUtil.NO) {
             return;
         } else {
-            const nodes: IBlockchainQuickPickItem<FabricNode>[] = await UserInputUtil.showFabricNodeQuickPick('Choose the nodes you wish to associate with this identity', environmentRegistryEntry.name, [], false, true, true) as IBlockchainQuickPickItem<FabricNode>[];
+            const nodes: IBlockchainQuickPickItem<FabricNode>[] = await UserInputUtil.showNodesInEnvironmentQuickPick('Choose the nodes you wish to associate with this identity', environmentRegistryEntry.name, [], false, true, true) as IBlockchainQuickPickItem<FabricNode>[];
 
             if (!nodes || nodes.length === 0) {
                 return;
