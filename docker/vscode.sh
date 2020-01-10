a5da07c1272edef28193ccd18c3cebd6835ad053
Simon Stone <sstone1@uk.ibm.com>
2020-01-10 13:34:55 +0000

    Update Docker image to Visual Studio Code v1.41.1 (resolves #1818)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- docker/vscode.sh
+++ docker/vscode.sh
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 set -ex
-if [ ! -r ~/.profile ]; then
-    tar cf - -C /etc/skel . | tar xf - -C ~
-fi
+FILES=$(cd /etc/skel && find . -type f)
+for FILE in ${FILES}; do
+    if [ ! -r ~/${FILE} ]; then
+        tar cf - -C /etc/skel ${FILE} | tar xf - -C ~
+    fi
+done
 exec dumb-init code-server --host 0.0.0.0 --extra-extensions-dir /usr/local/extensions --auth none
