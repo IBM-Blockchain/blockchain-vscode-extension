1422205252725f91e078731f9848dbc18ebc3973
Jake Turner <jaketurner25@live.com>
2020-03-17 10:24:43 +0000

    Updated to use Fabric 1.4.8 packages
    Signed-off-by: Jake Turner <jaketurner25@live.com>

--- packages/blockchain-extension/extension/util/ExtensionUtil.ts
+++ packages/blockchain-extension/extension/util/ExtensionUtil.ts
@@ -109,8 +109,8 @@ let blockchainPackageExplorerProvider: BlockchainPackageExplorerProvider;
 let blockchainEnvironmentExplorerProvider: BlockchainEnvironmentExplorerProvider;
 let blockchainWalletExplorerProvider: BlockchainWalletExplorerProvider;
 
-export const FABRIC_CLIENT_VERSION: string = '1.4.5';
-export const FABRIC_NETWORK_VERSION: string = '1.4.5';
+export const FABRIC_CLIENT_VERSION: string = '1.4.8';
+export const FABRIC_NETWORK_VERSION: string = '1.4.8';
 export const EXTENSION_ID: string = 'IBMBlockchain.ibm-blockchain-platform';
 
 // tslint:disable-next-line: max-classes-per-file
