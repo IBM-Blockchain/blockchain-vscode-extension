2cc3529a714c2895eb262a6cb20d60b5f1e920bc
Akshat Shah <47666989+au-shah@users.noreply.github.com>
2020-03-30 17:44:21 +0100

    Status page converted into TypeScript (#2058)
    Added Status Page
    
    Created status page website
    
    Signed-off-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>

---
+++ packages/blockchain-website/src/enzyme/CurrentVersion.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount, ReactWrapper } from 'enzyme';
+import chai from 'chai';
+import sinon from 'sinon';
+import Axios from 'axios';
+import sinonChai from 'sinon-chai';
+import CurrentVersion from '../components/CurrentVersion/CurrentVersion';
+chai.should();
+chai.use(sinonChai);
+
+const githubProject: string = 'IBM-Blockchain/blockchain-vscode-extension';
+
+// Github URLs
+const currentReleaseApiUrl: string = 'https://api.github.com/repos/' + githubProject + '/releases/latest';
+
+describe('CurrentVersion', () => {
+    let mySandBox: sinon.SinonSandbox;
+    let axiosGetStub: sinon.SinonStub;
+
+    beforeEach(async () => {
+        mySandBox = sinon.createSandbox();
+        axiosGetStub = mySandBox.stub(Axios, 'get');
+        axiosGetStub.returns({
+            data: {name: 'someVersion'}
+        });
+    });
+
+    afterEach(async () => {
+        mySandBox.restore();
+    });
+
+    it('should render the expected snapshot', async () => {
+        const component: any = renderer
+            .create(<CurrentVersion />)
+            .toJSON();
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should output latest version when successful api call', async () => {
+        const component: ReactWrapper<{}, {currentVersion: string}, CurrentVersion> = await mount(<CurrentVersion/>);
+        axiosGetStub.should.have.been.calledOnceWithExactly(currentReleaseApiUrl);
+        component.state().currentVersion.should.equal('someVersion');
+    });
+
+    it('should output nothing when unsuccessful api call', async () => {
+        axiosGetStub.returns(undefined);
+        const component: ReactWrapper<{}, {currentVersion: string}, CurrentVersion> = await mount(<CurrentVersion/>);
+        axiosGetStub.should.have.been.calledOnceWithExactly(currentReleaseApiUrl);
+        component.state().should.deep.equal({
+            currentVersion: ''
+        });
+    });
+});
