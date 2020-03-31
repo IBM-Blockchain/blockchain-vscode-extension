116c645d68c2ec86d1ff4934c5c8eb98ed4dda4a
Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>
2020-03-31 10:39:23 +0100

    Fixed label bug in status website
    Signed-off-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>

--- packages/blockchain-extension/test/commands/instantiateCommand.test.ts
+++ packages/blockchain-extension/test/commands/instantiateCommand.test.ts
@@ -245,6 +245,7 @@ describe('InstantiateCommand', () => {
         });
 
         it('should stop if cancelled when asked what chaincode EP to use', async () => {
+            executeCommandStub.resetHistory();
             showYesNo.resolves(UserInputUtil.NO);
             showQuickPick.resolves(undefined);
 
@@ -277,6 +278,7 @@ describe('InstantiateCommand', () => {
         });
 
         it(`should stop if user cancelled passing custom JSON CC EP file`, async () => {
+            executeCommandStub.resetHistory();
             showYesNo.onFirstCall().resolves(UserInputUtil.NO);
             showQuickPick.resolves(UserInputUtil.CUSTOM);
 
