fa9e6d5ed0dbd182a9b11e6ce9505b30f30a34f1
Akshat Shah <47666989+au-shah@users.noreply.github.com>
2020-04-02 16:25:27 +0100

    fixed bottom margin for status page (#2135)
    Signed-off-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>
    
    Co-authored-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3215309695c0592c11f318bebd00da7f.js"
+  "./precache-manifest.e0eae8c9225eee085f9eb51ce8c56765.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
