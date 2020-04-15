7dc3af28eef4f31d1ab6ee25058bbd3bf54d67fc
erin-hughes <35696678+erin-hughes@users.noreply.github.com>
2020-04-15 10:58:35 +0100

    Added functional tabs to tutorial view (#2184)
    Signed-off-by: Erin Hughes <Erin.Hughes@ibm.com>
    Co-authored-by: Akshat Shah <aksh282@gmail.com>
    
    Co-authored-by: Akshat Shah <aksh282@gmail.com>
    (cherry picked from commit eb29f662d134190087442974db322b2c50dda2a1)

---
+++ packages/blockchain-ui/src/components/elements/TutorialTabs/TutorialTabs.tsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Tabs, Tab } from 'carbon-components-react';
+import './TutorialTabs.scss';
+
+interface IProps {
+    tutorialData: Array<{seriesName: string, seriesTutorials: any[]}>;
+}
+
+class TutorialTabs extends Component<IProps> {
+
+    createTabs(): Array<JSX.Element> {
+        const tabArray: JSX.Element[] = [];
+        this.props.tutorialData.map((tutorialSeries: {seriesName: string, seriesTutorials: any[]}, index: number) => {
+            const tabLabel: string = `${tutorialSeries.seriesName} (${tutorialSeries.seriesTutorials.length})`;
+            tabArray.push(
+                // @ts-ignore
+                <Tab
+                    href='#'
+                    tabIndex={index}
+                    label={tabLabel}
+                >
+                    {this.populateTabs(tutorialSeries.seriesTutorials)}
+                </Tab>
+            );
+        });
+        return tabArray;
+    }
+
+    populateTabs(seriesTutorials: any[]): Array<JSX.Element> {
+        const tutorialNameArray: JSX.Element[] = [];
+        for (const tutorial of seriesTutorials) {
+            tutorialNameArray.push(
+                <p>{tutorial.title}</p>
+            );
+        }
+        return tutorialNameArray;
+    }
+
+    render(): JSX.Element {
+        let tabs: JSX.Element[] | JSX.Element;
+
+        if (this.props.tutorialData.length > 0) {
+            tabs = (
+                // @ts-ignore
+                <Tabs triggerHref='#' type='container'>
+                    {this.createTabs()}
+                </Tabs>
+            );
+        } else {
+            tabs = (<></>);
+        }
+
+        return (
+            <>
+                <div className='tutorial-tabs-background'></div>
+                {tabs}
+            </>
+        );
+    }
+}
+
+export default TutorialTabs;
