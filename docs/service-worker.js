de5f2bb777fc610575d2e9168bccccf5dc5f270f
Akshat Shah <47666989+au-shah@users.noreply.github.com>
2020-04-23 16:26:19 +0100

    updated website build (#2221)
    Signed-off-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>
    
    Co-authored-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9c44627a3203fef33541b832ae808122.js"
+  "./precache-manifest.81c2f92e789fb86a944ec6f8b2aa5abd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
