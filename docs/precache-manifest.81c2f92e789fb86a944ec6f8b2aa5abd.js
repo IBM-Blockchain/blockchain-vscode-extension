1f95703ada733d48111f4680dfddf774848d1d87
Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>
2020-04-23 15:47:47 +0100

    updated website build
    Signed-off-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>

---
+++ docs/precache-manifest.81c2f92e789fb86a944ec6f8b2aa5abd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b6b1ccb15c2dcafdb55389a80459ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f31d291bda1005af7e6f",
+    "url": "./static/css/main.c51f64b4.chunk.css"
+  },
+  {
+    "revision": "4807aa7b9b3e3db11005",
+    "url": "./static/js/2.2786bd39.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.2786bd39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31d291bda1005af7e6f",
+    "url": "./static/js/main.89264717.chunk.js"
+  },
+  {
+    "revision": "adbf2fba1bdf6a41d470",
+    "url": "./static/js/runtime-main.e9c66d1e.js"
+  },
+  {
+    "revision": "d8ba33e21a6ccf050eeabf9eb89d2834",
+    "url": "./static/media/add.d8ba33e2.svg"
+  },
+  {
+    "revision": "24529495a7281729e9aa02f95170e972",
+    "url": "./static/media/checkmark.24529495.svg"
+  },
+  {
+    "revision": "c5fb699b0b21b3035bef5f8b90ff18d9",
+    "url": "./static/media/error.c5fb699b.svg"
+  },
+  {
+    "revision": "0f5107487588e2447e283e998b05e6b4",
+    "url": "./static/media/warning.0f510748.svg"
+  }
+]);
