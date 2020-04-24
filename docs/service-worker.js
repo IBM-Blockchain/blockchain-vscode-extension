b7da8826fdd9c7dc6c785c6e56c0f2a4d6282872
Akshat Shah <47666989+au-shah@users.noreply.github.com>
2020-04-24 10:24:27 +0100

    updated website build (#2226)
    Signed-off-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>
    
    Co-authored-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e0eae8c9225eee085f9eb51ce8c56765.js"
+  "./precache-manifest.81c2f92e789fb86a944ec6f8b2aa5abd.js"
 );
 
 self.addEventListener('message', (event) => {
