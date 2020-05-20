79bf4f82e5e60a0c0caecb018f06cb613f4207e3
Simon Stone <sstone1@uk.ibm.com>
2020-05-20 12:01:27 +0100

    Fall back to file system when keytar not available (resolves #2310)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>
    (cherry picked from commit 1c89b9e9e1b20a6a73807d4f017c0bd8bdbd163e)
    
    # Conflicts:
    #	packages/blockchain-extension/test/commands/addEnvironmentCommand.test.ts

---
+++ packages/blockchain-extension/extension/util/SecureStoreFactory.ts
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+import { SecureStore } from './SecureStore';
+import { ModuleUtil } from './ModuleUtil';
+import { KeytarSecureStore } from './KeytarSecureStore';
+import { SettingConfigurations } from '../../configurations';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { FileSystemSecureStore } from './FileSystemSecureStore';
+
+export class SecureStoreFactory {
+
+    static async getSecureStore(): Promise<SecureStore> {
+        const keytar: any = ModuleUtil.getCoreNodeModule('keytar');
+        if (keytar) {
+            return new KeytarSecureStore(keytar);
+        }
+        const extensionDirectory: string = vscode.workspace.getConfiguration().get(SettingConfigurations.EXTENSION_DIRECTORY);
+        const storePath: string = path.join(extensionDirectory, 'ibm-blockchain-platform.store');
+        return new FileSystemSecureStore(storePath);
+    }
+
+}
